6f38c50e77663740b6cf8057a48163a5f9d20617
Bogdan Iudean <bogdan.iudean@gmail.com>
2021-03-03 19:28:55 +0200

    Initial commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        // stuff happening here
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithFalse()
+    {
+        // stuff happening here
+        assertFalse( false );
+    }
+
+}
